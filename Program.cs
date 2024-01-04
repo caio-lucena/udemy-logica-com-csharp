858a634a157b013b4340d1a6a82a96b7016cf049
caio-lucena <caio.lucena10@hotmail.com>
2024-01-04 20:25:25 -0300

    resolvendo um exercício básico para ir me acostumando com o C#

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// resolução do exercício proposto da aula 19
+
+using System;
+using System.Globalization;
 
 namespace PrimeiroProjeto
 {
@@ -6,15 +9,27 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Entre com o seu nome completo: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            int numberOfRooms = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double productPrice = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha)");
             string[] vector = Console.ReadLine().Split(' ');
 
-            for(int i = 0; i < vector.Length; i++)
+            Console.WriteLine(name);
+            Console.WriteLine(numberOfRooms);
+            Console.WriteLine(productPrice.ToString("F2", CultureInfo.InvariantCulture));
+            
+            for (int i = 0; i < vector.Length; i++)
             {
-                Console.Write(vector[i] + " ");
+                Console.WriteLine(vector[i]);
             }
-            
-            Console.WriteLine();
-            
+
         }
     }
 }
